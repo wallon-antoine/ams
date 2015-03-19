0b5938623f2f50a6a9bd994ca68b39c4a9b01386
Antoine WALLON <antoine.wallon@univ-lille1.fr>
2015-03-19 15:33:08 +0100

    Add $CI propriety

--- application/libraries/ldap.php
+++ application/libraries/ldap.php
@@ -2,24 +2,26 @@
 
 class Ldap {
 
+        private $_CI = null;
+    
+    
 	public function __construct(){
 		if (!function_exists('ldap_bind')){
 			show_error('<strong>ERROR:</strong> You need to install the PHP module
 				<strong><a href="http://php.net/ldap">ldap</a></strong> to be able
 				to use ldap librarie.');
 		}
-                $CI =& get_instance();
-		$this->CI = $CI;      
+                $this->_CI=& get_instance();     
 
         }
         
         private function connect() {
       
-            $connect = ldap_connect($this->CI->config->item('ldapuri'), $this->CI->config->item('ldapport'));
+            $connect = ldap_connect($this->_CI->config->item('ldapuri'), $this->_CI->config->item('ldapport'));
             
             ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3);
             ldap_set_option($connect, LDAP_OPT_REFERRALS, 0);;
-            ldap_bind($connect, $this->CI->config->item('binddn'), $this->CI->config->item('bindpw'));
+            ldap_bind($connect, $this->_CI->config->item('binddn'), $this->_CI->config->item('bindpw'));
             
             return $connect;
         }
@@ -27,7 +29,7 @@ class Ldap {
         
         public function search_entry($filter,$type) {
             $justthese = array("sn", "givenname", "uid");
-            $read = ldap_search($this->connect(), "ou=People,".$this->CI->config->item('basedn'), $type."=".$filter,$justthese);
+            $read = ldap_search($this->connect(), "ou=People,".$this->_CI->config->item('basedn'), $type."=".$filter,$justthese);
             $info = ldap_get_entries($this->connect(), $read);
             if ($info != NULL) {
                 return $info;
@@ -45,7 +47,7 @@ class Ldap {
                 //$filter    = "$person*";
                 $filter="(|(uid=*$person*))";
                 $justthese = array("sn", "givenname", "uid");
-                $sr        = ldap_search($this->connect(),"ou=People,".$this->CI->config->item('basedn'), $filter,$justthese);
+                $sr        = ldap_search($this->connect(),"ou=People,".$this->_CI->config->item('basedn'), $filter,$justthese);
                 $info      = ldap_get_entries($this->connect(), $sr);
                 $result = array();
                 if ($info != NULL) {
